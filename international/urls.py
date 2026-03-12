5fd3067c19fbd8bdd58a427bc54f27fb2ecd17ed
Hephzibah Ofomi <h.ofomi@alustudent.com>
2026-03-12 23:31:33 +0000

    re-uploaded summative for the 5th time

--- international/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # International shipments
-    path('shipments/', views.InternationalShipmentListCreateView.as_view(), name='intl-shipment-list-create'),
-    path('shipments/<str:tracking_number>/', views.InternationalShipmentDetailView.as_view(), name='intl-shipment-detail'),
-    path('shipments/<str:tracking_number>/tracking/', views.InternationalShipmentTrackingView.as_view(), name='intl-shipment-tracking'),
-]
